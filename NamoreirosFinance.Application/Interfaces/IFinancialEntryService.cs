ddd5d976f83bf1d4384221804b7cb0c79a9721fc
Mateus Antonio <mateusantonio23@live.com>
2024-12-16 21:18:24 -0300

    created application layer w/ service class

---
+++ NamoreirosFinance.Application/Interfaces/IFinancialEntryService.cs
@@ -0,0 +1,13 @@
+﻿using NamoreirosFinance.Domain.Core.Entities.Transaction;
+
+namespace NamoreirosFinance.Application.Interfaces
+{
+    public interface IFinancialEntryService
+    {
+        Task<FinancialEntry> CreateAsync(FinancialEntry financialEntry);
+        Task<FinancialEntry> GetByIdAsync(int id);
+        Task<List<FinancialEntry>> GetAllAsync();
+        Task UpdateAsync(FinancialEntry financialEntry);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ NamoreirosFinance.Application/Services/FinancialEntryService.cs
@@ -0,0 +1,42 @@
+﻿using NamoreirosFinance.Domain.Core.Entities.Transaction;
+using NamoreirosFinance.Domain.Core.Repositories;
+
+namespace NamoreirosFinance.Application.Services
+{
+    public class FinancialEntryService : IFinancialEntryRepository
+    {
+        private static IFinancialEntryRepository _repository;
+
+        public FinancialEntryService(IFinancialEntryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Add(FinancialEntry financialEntry)
+        {
+            await _repository.Add(financialEntry);
+        }
+
+        public async Task Delete(FinancialEntry financialEntry)
+        {
+            await _repository.Delete(financialEntry);
+        }
+
+        public async Task<List<FinancialEntry>> GetAll()
+        {
+            var entries = await _repository.GetAll();
+            return entries;
+        }
+
+        public async Task<FinancialEntry> GetById(int id)
+        {
+            var entry = await _repository.GetById(id);
+            return entry;
+        }
+
+        public async Task Update(FinancialEntry financialEntry)
+        {
+            _repository.Update(financialEntry);
+        }
+    }
+}
