ac3d6ada65a1643d6b7a5fc27f2651cc7d638ef9
Mateus Antonio <mateusantonio23@live.com>
2025-02-06 19:14:07 -0300

    removed angular material and kinda restarted doing the front end

--- NamoreirosFinance.WebApp/ClientApp/src/app/components/registration-content-box/registration-content-box.component.ts
+++ NamoreirosFinance.WebApp/ClientApp/src/app/components/registration-content-box/registration-content-box.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {MatInputModule} from '@angular/material/input'; 
+import {MatFormFieldModule} from '@angular/material/form-field'; 
 
 @Component({
   selector: 'app-registration-content-box',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule],
   templateUrl: './registration-content-box.component.html',
   styleUrl: './registration-content-box.component.scss'
 })
 export class RegistrationContentBoxComponent {
-
+  @Input({required: true, alias: 'box-title-text'}) titleText: string = "";
 }
--- NamoreirosFinance.WebApp/ClientApp/src/app/pages/financial-entry-registration/financial-entry-registration.component.ts
+++ NamoreirosFinance.WebApp/ClientApp/src/app/pages/financial-entry-registration/financial-entry-registration.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { AppMainbackgroundComponent } from "../../components/app-main-layout/app-main-background.component";
 import { RegistrationContentBoxComponent } from "../../components/registration-content-box/registration-content-box.component";
 
 @Component({
   selector: 'app-financial-entry-registration',
   standalone: true,
-  imports: [AppMainbackgroundComponent, RegistrationContentBoxComponent],
+  imports: [RegistrationContentBoxComponent],
   templateUrl: './financial-entry-registration.component.html',
   styleUrl: './financial-entry-registration.component.scss'
 })
