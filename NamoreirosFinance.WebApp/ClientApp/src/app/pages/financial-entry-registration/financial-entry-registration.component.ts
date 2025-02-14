ecbf5bb958c2156521103f4a4c6104062178582d
Mateus Antonio <mateusantonio23@live.com>
2025-02-13 21:59:51 -0300

    started doing forms validation

--- NamoreirosFinance.WebApp/ClientApp/src/app/pages/financial-entry-registration/financial-entry-registration.component.ts
+++ NamoreirosFinance.WebApp/ClientApp/src/app/pages/financial-entry-registration/financial-entry-registration.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RegistrationContentBoxComponent } from "../../components/registration-content-box/registration-content-box.component";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-financial-entry-registration',
   standalone: true,
-  imports: [RegistrationContentBoxComponent],
+  imports: [RegistrationContentBoxComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './financial-entry-registration.component.html',
   styleUrl: './financial-entry-registration.component.css'
 })
 export class FinancialEntryRegistrationComponent {
+  financialEntryForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.financialEntryForm = this.formBuilder.group({
+      transactionDescription: ["", Validators.required],
+      transactionValue: ["", [Validators.required, Validators.min(0.01), Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
+      transactionDate: ["", Validators.required],
+      transactionType: ["Entrada", Validators.required]
+    })
+  }
+  
 }
